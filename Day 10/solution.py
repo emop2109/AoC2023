0bb601af6e00fe93a05022728caaa18e21b9e0ab
Emil <emil.oppegard@hafslundeco.no>
2023-12-11 16:06:49 +0100

    Day 10

--- Day 10/solution.py
+++ Day 10/solution.py
@@ -16,7 +16,7 @@ class Vertex:
 
 grid = [''.join([maps[d][i] for d in line]) for line in input for i in range(0,3)]
 start = Vertex([(r,c) for r in range(len(grid)) for c in range(len(grid[0])) if grid[r][c] == 'S'][0], None, 0)
-current = [(r,c)]
+current = [(start.position[0] - 2*i,start.position[1]) for i in [-1, 1] if grid[start.position[0] - 2*i][start.position[1]] in '|-']
 # ---------------------------------------- Del 1 -------------------------------------------
 
 # ---------------------------------------- Del 2 -------------------------------------------
