f6f75c2c26e4ab3bc2f07600b6f62ac8ddfe7a8a
Emil <emil.oppegard@hafslundeco.no>
2023-12-13 16:48:15 +0100

    Day 12

--- Day 12/solution.py
+++ Day 12/solution.py
@@ -1,28 +1,42 @@
+import re
+
 with open('.\input.txt', 'r') as file:
     input=file.read().splitlines()
 
-class Spring:
-    def __init__(self, type):
-        self.type = type
-        self.next = []
+def valid_character(string, remaining_string, next_character, sequence):
+    occurences = re.findall(r'#+', string+next_character)
+    if len(occurences):
+        if len(occurences) > len(sequence):
+            return False
+        elif any(len(occurences[i]) != sequence[i] for i in range(len(occurences) - 1)):
+            return False
+        elif len(occurences[-1]) > sequence[len(occurences) - 1]:
+            return False
+        elif sum(sequence) - sum([len(x) for x in occurences]) > len(remaining_string):
+            return False
+    return True
+def generate_string(remaining_string, current_string, sequence):
+    if len(remaining_string) == 0:
+        occurences = re.findall(r'#+', current_string)
+        return 1 if len(occurences) == len(sequence) and all([len(occurences[i]) == sequence[i] for i in range(len(occurences))]) else 0
+    elif remaining_string[0] != '?':
+        return generate_string(remaining_string[1:], current_string+remaining_string[0], sequence)
+    else:
+        return sum([generate_string(remaining_string[1:], current_string+q, sequence) if valid_character(current_string, remaining_string[1:], q, sequence) else 0 for q in ['#', '.']])
+
 # ---------------------------------------- Del 1 -------------------------------------------
-def build_tree(spring, line):
-    if len(line):
-        if line[0] == '?':
-            _spring_alt_1, _spring_alt_2 = Spring('.'), Spring('#')
-            spring.next.extend([_spring_alt_1, _spring_alt_2])
-            build_tree(_spring_alt_1, line[1:])
-            build_tree(_spring_alt_2, line[1:])
-        else:
-            _spring_ = Spring(line[0])
-            spring.next.append(_spring_)
-            build_tree(_spring_, line[1:])
-def get_string(spring, msg):
-starts = []
+_sum_ = 0
 for line in input:
-    spring = Spring(None)
-    starts.append(spring)
-    build_tree(spring, line.split(' ')[0])
+    d = [int(x) for x in re.findall(r'\d+', line)]
+    _sum_ += generate_string(line.split(' ')[0], '', d)
+print(_sum_)
 
 # ---------------------------------------- Del 2 -------------------------------------------
-
+_sum_, _ = 0,0
+for line in input:
+    d = [int(x) for x in re.findall(r'\d+', line)]
+    string = line.split(' ')[0] + '?' + line.split(' ')[0] + '?' + line.split(' ')[0] + '?' + line.split(' ')[0] + '?' + line.split(' ')[0]
+    _ = generate_string(string, '', [x for i in [0,1,2,3,4] for x in d])
+    print(string, _)
+    _sum_ += _
+print(_sum_)
