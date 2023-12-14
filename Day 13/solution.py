0edbaf0aec8586d9d5603307efefd4431c575427
Emil <e.oppegaard@gmail.com>
2023-12-14 20:39:10 +0100

    Day 13

--- Day 13/solution.py
+++ Day 13/solution.py
@@ -33,17 +33,25 @@ for key in maps:
 print(rows*100+columns)
 
 # ---------------------------------------- Del 2 -------------------------------------------
-def compare_segments_version_2(pattern, left_index, right_index):
+def compare_segments_version_2(pattern, left_index, right_index, checked=False):
     if left_index < 0 or right_index == len(pattern):
         return False
     elif pattern[left_index] == pattern[right_index]:
         return compare_segments_version_2(pattern, left_index - 1, right_index + 1)
-    elif abs(pattern[left_index].count('#') - pattern[right_index].count('#')) == 1:
-        return not compare_segments_version_2(pattern, left_index - 1, right_index + 1)
+    elif sum([1 if pattern[left_index][i] != pattern[right_index][i] else 0 for i in range(len(pattern[left_index]))]) == 1 and not checked:
+        return not compare_segments_version_2(pattern, left_index - 1, right_index + 1, True)
     else:
-        return False
+        return checked
+def get_rows_version_2(pattern):
+    for j in range(len(pattern) - 1):
+        if sum([1 if pattern[j][i] != pattern[j+1][i] else 0 for i in range(len(pattern[j]))]) == 1:
+            if compare_segments(pattern, j - 1, j + 2):
+                return j + 1
+        elif pattern[j] == pattern[j+1]:
+            if compare_segments_version_2(pattern, j - 1, j + 2):
+                return j + 1
+    return 0
 rows, columns = 0,0
 for key in maps:
-    rows += get_rows(maps[key], compare_segments_version_2)
-    columns += get_rows([''.join(row) for row in zip(*maps[key])], compare_segments_version_2)
+    rows += get_rows_version_2(maps[key])
 print(rows*100+columns)
