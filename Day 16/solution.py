778e6a88fceab8770f15685fd894069d4ec24104
Emil <e.oppegaard@gmail.com>
2023-12-16 23:18:04 +0100

    Day 16

--- Day 16/solution.py
+++ Day 16/solution.py
@@ -1,30 +1,37 @@
+from functools import cache
+
 with open('.\input.txt', 'r') as file:
     input=file.read().splitlines()
 
-def depth_first_search(start_position):
-    visited, queue = [], [start_position]
+@cache
+def calculate_next_position(row, column, row_dir, col_dir, character):
+    match character:
+        case '\\':
+            return [(row + col_dir, column + row_dir, col_dir, row_dir)]
+        case '/':
+            return [(row - col_dir, column - row_dir, -col_dir, -row_dir)]
+        case '|':
+            return [(row + col_dir, column, col_dir, 0), 
+                    (row - col_dir, column, -col_dir, 0)
+                    if col_dir != 0 else (row + row_dir, column, row_dir, col_dir)]
+        case '-':
+            return [(row, column + row_dir, 0, row_dir), 
+                    (row, column - row_dir, 0, -row_dir) 
+                    if row_dir != 0 else (row, column + col_dir, row_dir, col_dir)]
+        case '.':
+            return [(row + row_dir, column + col_dir, row_dir, col_dir)]
+          
+def depth_first_search(starting_position):
+    visited, queue = set(), [starting_position]
     while len(queue):
-        position, directions = queue.pop()
-        if (position,directions) not in visited:
-            if -1 < position[0] < len(input) and -1 < position[1] < len(input[0]):
-                if input[position[0]][position[1]] == '\\':
-                    queue.append(((position[0] + directions[1], position[1] + directions[0]), (directions[1], directions[0])))
-                elif input[position[0]][position[1]] == '/':
-                    queue.append(((position[0] - directions[1], position[1] - directions[0]), (-directions[1], -directions[0])))
-                elif input[position[0]][position[1]] == '|' and directions[1] != 0:
-                    queue.append(((position[0] - directions[1], position[1] - directions[0]), (-directions[1], -directions[0])))
-                    queue.append(((position[0] + directions[1], position[1] + directions[0]), (directions[1], directions[0])))
-                elif input[position[0]][position[1]] == '-' and directions[0] != 0:
-                    queue.append(((position[0] - directions[1], position[1] - directions[0]), (-directions[1], -directions[0])))
-                    queue.append(((position[0] + directions[1], position[1] + directions[0]), (directions[1], directions[0])))
-                else:
-                    queue.append(((position[0] + directions[0], position[1] + directions[1]),directions))
-                visited.append((position, directions))
-    return visited
+        (row, column, row_dir, col_dir) = queue.pop()
+        if (row, column, row_dir, col_dir) not in visited and -1 < row < len(input) and -1 < column < len(input[0]):
+            queue.extend(calculate_next_position(row, column, row_dir, col_dir, input[row][column]))
+            visited.add((row, column, row_dir, col_dir))
+    return len(set((x,y) for x, y, row_dir, col_dir in visited))
 # ---------------------------------------- Del 1 -------------------------------------------
-visited = set([x[0] for x in depth_first_search(((0,0), (0,1)))])
-print(len(visited))
+print(depth_first_search((0,0,0,1)))
 # ---------------------------------------- Del 2 -------------------------------------------
-vertical = max([len(set([x[0] for x in depth_first_search(((len(input)*n, c),((-1)**n,0)))])) for n in [0, 1] for c in range(len(input[0]))])
-horizontal = max([len(set([x[0] for x in depth_first_search(((r, len(input[0])*n),(0,(-1)**n)))])) for n in [0, 1] for r in range(len(input))])
+vertical = max([depth_first_search((len(input)*n, c, (-1)**n,0)) for n in [0, 1] for c in range(len(input[0]))])
+horizontal = max([depth_first_search((r, len(input[0])*n,0,(-1)**n)) for n in [0, 1] for r in range(len(input))])
 print(max(vertical, horizontal))
